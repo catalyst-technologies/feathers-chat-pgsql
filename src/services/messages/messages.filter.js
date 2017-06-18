264ea8b05097d9e574626fd728989399ebf9760b
Mark Clu Heramis <chumheramis@gmail.com>
2017-06-19 05:54:24 +0800

    fix and add log in filters

--- src/services/messages/messages.filter.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  created(data, connection, hook) {
-    console.log(data);
-    console.log(connection);
-    console.log(hook);
-  },
-  updated(data, connection, hook) {}
-}
--- src/services/messages/messages.filters.js
+++ src/services/messages/messages.filters.js
@@ -2,5 +2,7 @@
 console.warn('You are using the default filter for the messages service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering'); // eslint-disable-line no-console
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
+  console.log('message filters');
+  console.log(data);
   return data;
 };
