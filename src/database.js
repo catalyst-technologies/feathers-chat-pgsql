b50f8c2174e143d011e956051348cb33c59a2046
Cyril Lonsido <cy.lonsido@gmail.com>
2017-06-18 19:11:38 +0800

    corrected - postgres config

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 module.exports = function () {
 	const app = this;
-	const config = app.get('db_config');
+	const config = app.get('postgres');
 	const db = knex(config);
 	app.set('database', db);
 };
